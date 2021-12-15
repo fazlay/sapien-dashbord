60e9b2350d82e4fe057f5a24da188446caafbffd
fazlay <rfazlay21@gmail.com>
2021-12-15 10:25:57 +0600

    list badge

--- src/Component/TableComponent.js
+++ src/Component/TableComponent.js
@@ -1,10 +1,44 @@
-import React from "react";
-import { Dropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import { Badge, Dropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import "../App.css";
 
 const TableComponent = () => {
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      <FontAwesomeIcon icon={faEllipsisV} />
+    </a>
+  ));
+
+  const CustomMenu = React.forwardRef(
+    ({ children, style, className, "aria-labelledby": labeledBy }, ref) => {
+      const [value, setValue] = useState("");
+
+      return (
+        <div
+          ref={ref}
+          style={style}
+          className={className}
+          aria-labelledby={labeledBy}
+        >
+          <ul className="list-unstyled">
+            {React.Children.toArray(children).filter(
+              (child) =>
+                !value || child.props.children.toLowerCase().startsWith(value)
+            )}
+          </ul>
+        </div>
+      );
+    }
+  );
   return (
     <div>
       <table class="table">
@@ -21,28 +55,36 @@ const TableComponent = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">May 26, 2019</th>
-            <td>Shaivam Bhwal</td>
-            <td>+91987654321</td>
-            <td>dummy@gmail.com</td>
-            <td>website</td>
-            <td>25 June 2021</td>
-            <td> New</td>
-            <td>
+          <tr className=" shadow rounded-3 py-4 ">
+            <th className="py-4 position-relative " scope="row">
+              <span class="badge px-4 rounded-top badge-notification top-0 start-0 bg-danger position-absolute">
+                Not Seen
+              </span>
+              May 26, 2019
+              <br />
+              <span className="fw-light pt-2"> 8.00 AM</span>
+            </th>
+            <td className="py-4">Shaivam Bhwal</td>
+            <td className="py-4">+91987654321</td>
+            <td className="py-4">dummy@gmail.com</td>
+            <td className="py-4">website</td>
+            <td className="py-4">25 June 2021</td>
+            <td className="py-4"> New</td>
+            <td className="py-4">
               <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic" varient={light}>
-                  <FontAwesomeIcon icon={faEllipsisV} />
+                <Dropdown.Toggle
+                  as={CustomToggle}
+                  id="dropdown-custom-components"
+                >
+                  Custom toggle
                 </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">
-                    Another action
+                <Dropdown.Menu as={CustomMenu}>
+                  <Dropdown.Item eventKey="1" active>
+                    Edit
                   </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">
-                    Something else
-                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="2">View</Dropdown.Item>
+                  <Dropdown.Item eventKey="3">Delete</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </td>
