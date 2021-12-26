b6c418ddd7164bbf6a5e8686f3bb9b8c3576e4e0
fazlay <rfazlay21@gmail.com>
2021-12-27 00:32:52 +0600

    cmomit

--- src/Component/TableComponent.js
+++ src/Component/TableComponent.js
@@ -1,10 +1,48 @@
-import React from "react";
-import { Dropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import { Dropdown, FormControl } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import "../App.css";
 
 const TableComponent = () => {
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+    
+     <FontAwesomeIcon icon={faEllipsisV}/>
+    </a>
+  ));
+  
+  // forwardRef again here!
+  // Dropdown needs access to the DOM of the Menu to measure it
+  const CustomMenu = React.forwardRef(
+    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+      const [value, setValue] = useState('');
+  
+      return (
+        <div
+          ref={ref}
+          style={style}
+          className={className}
+          aria-labelledby={labeledBy}
+        >
+       
+          <ul className="list-unstyled">
+            {React.Children.toArray(children).filter(
+              (child) =>
+                !value || child.props.children.toLowerCase().startsWith(value),
+            )}
+          </ul>
+        </div>
+      );
+    },
+  );
   return (
     <div>
       <table class="table">
@@ -30,21 +68,20 @@ const TableComponent = () => {
             <td>25 June 2021</td>
             <td> New</td>
             <td>
-              <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic" varient={light}>
-                  <FontAwesomeIcon icon={faEllipsisV} />
-                </Dropdown.Toggle>
+            <Dropdown>
+    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+      Custom toggle
+    </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">
-                    Another action
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">
-                    Something else
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+    <Dropdown.Menu as={CustomMenu}>
+      <Dropdown.Item eventKey="1" active>Edit</Dropdown.Item>
+      <Dropdown.Item eventKey="2">View</Dropdown.Item>
+      <Dropdown.Item eventKey="3" >
+        Delete
+      </Dropdown.Item>
+    
+    </Dropdown.Menu>
+  </Dropdown>
             </td>
           </tr>
         </tbody>
